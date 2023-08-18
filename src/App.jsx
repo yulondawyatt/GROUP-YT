16d6d169f971fb8129fac18fa710c2b42c0f3b3e
Oscar Quintanilla <oscarquintanilla@pursuit.org>
2023-08-18 13:09:28 -0400

    added apiKey variable

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ import SearchBar from './Components/SearchBar.jsx'
 import './App.css'
 
 function App() {
-
+  const apiKey = import.meta.env.VITE_API_KEY
+  
+  fetch(`https://youtube.googleapis.com/youtube/v3/search?key=${apiKey}`)
+    .then(r => r.json())
+    .then(console.log)
   return (
     <>
+      
       <SearchBar />
     </>
   )
